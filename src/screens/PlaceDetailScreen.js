d8a65ab938f20cab6e4a2e84407dafc5829932f9
Mariana Soltis <marianabs_15@yahoo.com.ar>
2023-04-30 17:34:38 -0300

    location con estilos

--- src/screens/PlaceDetailScreen.js
+++ src/screens/PlaceDetailScreen.js
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import MapPreview from '../components/MapPreview'
+import { useSelector } from 'react-redux'
+import { COLORS } from '../constants/Colors'
 
-const PlaceDetailScreen = () => {
+const PlaceDetailScreen = ({ route }) => {
+  const { placeId } = route.params
+
+  const selectedPlace = useSelector(state => state.places.places.find(place => place.id === placeId))
+
   return (
-    <View>
-      <Text>PlaceDetailScreen</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedPlace.image }} style={styles.image} />
+      <View style={styles.location}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.addressContainer}>{selectedPlace.address}</Text>
+        </View>
+        <MapPreview style={styles.map} location={{ lat: selectedPlace.lat, lng: selectedPlace.lng }} >
+          <Text>{selectedPlace.title}</Text>
+        </MapPreview>
+      </View>
+    </ScrollView>
   )
 }
 
 export default PlaceDetailScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  image: {
+    height: '35%',
+    minHeight: 300,
+    width: '100%'
+  },
+  location: {
+    margin: 20,
+    width: '90%',
+    maxWidth: 350,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10
+  },
+  addressContainer: {
+    padding: 20
+  },
+  address: {
+    color: COLORS.celeste,
+    textAlign: 'center'
+  },
+  map: {
+    height: 300,
+  }
+})
